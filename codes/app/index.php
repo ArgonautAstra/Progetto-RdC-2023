c2677a1135eec869510b0b269cbc22dce265c76c
ArgonautAstra <78497325+ArgonautAstra@users.noreply.github.com>
2022-12-06 11:10:03 +0100

    Add files via upload

---
+++ codes/app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>BRUTTU</title>
+	
+	</head>
+	
+	<body>
+		<form method="POST" action="fake_login.php">
+			Username: <input type="text" name="usr"><br>
+			Password: <input type="password" name="psw"><br>
+			<input type="submit" value="Login">
+		</form>
+		 <iframe src="https://www.fabiotropia.it" style="width:50%; height:500px"></iframe> 
+		 <iframe src="home.php"  style="width:50%; height:500px"></iframe> 
+	</body>
+	
+	<scrypt>
+	
+	</scrypt>
+</html>
