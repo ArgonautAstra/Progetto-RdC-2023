8c1083026bd11f3ff91ab8a4c921349e5de8df9e
marco <gamespy67@gmail.com>
2023-01-21 12:17:09 +0100

    set Ejs view engine

--- app/index.js
+++ app/index.js
@@ -1,19 +1,24 @@
 const express = require('express')
-
-const database = require("./db/Db")
-
+const database = require("./src/Backend/db/Db")
 const app = express();
-
-//const { user: UserModel } = require('./db/sequelize');
-
 const data = database.getDB()
-const data2 = database.getDB()
+const port = 3000;
+
+
+
+app.use(express.static('public'));
+
+app.set('views', './src/views')
+app.set("view engine", "ejs")
 
 app.route('/').get(async (req, res) => {
-    res.send(data === data2)
+	res.render('index', {
+		title: 'Web Application using Node.js',
+		heading: 'Hello C# Corner, Welcome to Node.js Tutorial',
+		foodItems: ['Pizza', 'Burger', 'Pasta']
+	});
 });
 
-const port = 3000;
 app.listen(port, () => {
 	console.log(`Worker: process ${process.pid} is up on port ${port}`);
 });
